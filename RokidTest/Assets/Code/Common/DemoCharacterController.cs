5071f08e2ed73a486532e74fa9764250eb34d6a5
Peitao Zhao <zpt0219@gmail.com>
2018-07-09 22:18:55 -0700

    implement the vuforia

--- RokidTest/Assets/Code/Common/DemoCharacterController.cs
+++ RokidTest/Assets/Code/Common/DemoCharacterController.cs
@@ -50,6 +50,18 @@ namespace Rokid
 		}
 
 		// Update is called once per frame
+
+		private void OnGUI()
+		{
+            GUI.Label(new Rect(10, 10, 100, 30), Input.acceleration.x.ToString());
+            GUI.Label(new Rect(10, 40, 100, 30), Input.acceleration.y.ToString());
+            GUI.Label(new Rect(10, 70, 100, 30), Input.acceleration.z.ToString());
+            GUI.Label(new Rect(10, 100, 100, 30), m_cameraPivot.localEulerAngles.x.ToString());
+            GUI.Label(new Rect(10, 130, 100, 30), m_cameraPivot.localEulerAngles.y.ToString());
+
+
+		}
+
 		void Update()
 		{
 			//nextAnimationEnum = AnimationEnum.Idle;
@@ -114,9 +126,10 @@ namespace Rokid
 
 		private void LateUpdate()
 		{
-			Vector3 dir = Input.mousePosition - lastMousePosition;
-			lastMousePosition = Input.mousePosition;
-			UpdateCamera(dir.x, -dir.y);
+            UpdateCameraByGravity();
+			//Vector3 dir = Input.mousePosition - lastMousePosition;
+			//lastMousePosition = Input.mousePosition;
+            //UpdateCamera(dir.x, -dir.y);
 			
 			SetAnimation(nextAnimationEnum);
 		}
@@ -148,6 +161,33 @@ namespace Rokid
 			}
 		}
 
+        List<Vector2> m_degreeFilter = new List<Vector2>();
+
+        private void UpdateCameraByGravity()
+        {
+            if (m_lockCamera) return;
+            float xAngle = Mathf.Asin(Input.acceleration.y);
+            float yAngle = Mathf.Asin(Input.acceleration.x);
+            xAngle = xAngle * 180 / Mathf.PI;
+            if (xAngle > 180) xAngle -= 360;
+            yAngle = yAngle * 180 / Mathf.PI;
+            if (yAngle > 180) yAngle -= 360;
+            xAngle = Mathf.RoundToInt(xAngle);
+            yAngle = Mathf.RoundToInt(yAngle);
+
+            m_degreeFilter.Add(new Vector2(xAngle, yAngle));
+            if (m_degreeFilter.Count < 6) return;
+            m_degreeFilter.RemoveAt(0);
+
+            /*Vector2 v = 0.0009f * m_degreeFilter[0] + 0.0175f * m_degreeFilter[1] + 0.1295f * m_degreeFilter[2] +
+                         0.3521f * m_degreeFilter[3] + 0.3521f * m_degreeFilter[4] + 0.1295f * m_degreeFilter[5] +
+                         0.0175f * m_degreeFilter[6] + 0.009f * m_degreeFilter[7];*/
+
+            Vector2 v = m_degreeFilter[0] + 4 * m_degreeFilter[1] + 6 * m_degreeFilter[2] + 4 * m_degreeFilter[3] + m_degreeFilter[4];
+            v /= 16;
+
+            m_cameraPivot.localEulerAngles = new Vector3(v.x, v.y, 0);
+        }
 
 		private void UpdateCamera(float deltaX, float deltaY)
 		{
@@ -165,8 +205,8 @@ namespace Rokid
 					}
 				}
 			}
-			Transform cameraTrans = m_cameraPivot.GetChild(0);
-			float z = Mathf.Clamp(Input.mouseScrollDelta.y * 0.3f + cameraTrans.localPosition.z, -8, -2);
+			//Transform cameraTrans = m_cameraPivot.GetChild(0);
+			//float z = Mathf.Clamp(Input.mouseScrollDelta.y * 0.3f + cameraTrans.localPosition.z, -8, -2);
 			//cameraTrans.localPosition = new Vector3(cameraTrans.localPosition.x, cameraTrans.localPosition.y, z);
 		}
 
